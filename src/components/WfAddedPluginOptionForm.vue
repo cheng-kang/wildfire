dc2227771693fcc29c903ada6215a8d62c446b71
Kang Cheng <me@chengkang.pw>
2018-03-14 22:15:43 +0000

    feature/update plugin options

--- src/components/WfAddedPluginOptionForm.vue
+++ src/components/WfAddedPluginOptionForm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="wf-added-plugin-option-form">
-    <i-form v-if="form" :model="form" :rules="rules" label-position="top">
+    <i-form v-if="form" ref="form" :model="form" :rules="rules" label-position="top">
       <i-form-item v-for="option in options" :key="option.key" :label="t(option.displayName)" :prop="option.key">
         <i-input v-model="form[option.key]" :placeholder="t(option.placeholder)"></i-input>
       </i-form-item>
       <div>
-        <i-button type="primary" @click="submit">Submit</i-button>
+        <i-button type="primary" @click="submit" :loading="isUpdating">Submit</i-button>
         <i-button type="ghost" @click="reset" style="margin-left: 8px">Reset</i-button>
       </div>
     </i-form>
@@ -22,14 +22,15 @@ export default {
     return {
       form: undefined,
       rules: undefined,
+      isUpdating: false,
       t: PTM.t(Bus.config.locale)(this.pluginId),
     };
   },
   computed: {
-    db: () => Bus.db,
+    optionRef: () => Bus.db.ref(`plugins/${this.pluginId}/a/x/x/x/u/options`),
   },
   created() {
-    this.db.ref(`plugins/${this.pluginId}/a/x/x/x/u/options`).once('value')
+    this.optionRef.once('value')
       .then(snapshot => {
         const form = {};
         const rules = {};
@@ -54,10 +55,28 @@ export default {
       });
   },
   methods: {
-    submit: () => {
-      console.log('submit');
+    submit() {
+      this.isUpdating = true;
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.optionRef.update(this.form)
+            .then(() => {
+              this.$Message.success('AddedPluginOptionForm.success.update');
+            })
+            .catch(error => {
+              console.error(error);
+              this.$Message.error('AddedPluginOptionForm.error.update');
+            })
+            .finally(() => {
+              this.isUpdating = false;
+            });
+        } else {
+          this.isUpdating = false;
+          this.$Message.error('AddedPluginOptionForm.error.invalid_form');
+        }
+      });
     },
-    reset: () => {
+    reset() {
       Object.keys(this.form).forEach(key => {
         this.$set(this.form, key, '');
       });
